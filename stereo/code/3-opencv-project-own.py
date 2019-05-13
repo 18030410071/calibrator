b2e16a4e809196cdeb31369650f9724334470337
hp-lius <2904500451@qq.com>
2019-05-13 20:10:55 +0800

    add

--- stereo/code/3-opencv-project-own.py
+++ stereo/code/3-opencv-project-own.py
@@ -2,38 +2,19 @@ import cv2
 import glob
 import numpy as np
 
-# From matlab 
-mtx = np.array([[1147.48828419389,	0,	0],
-                [0.847281482146116,	1154.12643432534,	0],
-                [413.839306493625,	125.934874029344,	1]]).T
-            
-mtx_r = np.array([[1191.78793540320,	0,	0],
-                 [3.15794233942629,	1201.09936716930,	0],
-                 [298.930379027692,	119.282685914931,	1]]).T
 
-R = np.array([[0.898066037373019,	0.0213408993926283,	-0.439342643650985],
-              [-0.0214251464935368,	0.999759088048663,	0.00476749009820505],
-              [0.439338543283939,	0.00513145956036998,	0.898306914427317]])
+cameraMatrix1 = None
+cameraMatrix2 = None
 
-T = np.array([-264.886066592313,	-1.77392898927413,	46.7689011903979])
-
-# K [-0.404240685876510,0.679412484741657,-4.804320406255162]
-# P [0.003558952648167,-1.745438223176216e-04]
-# [K1 K2 P1 P2 K3]
-distCoeffs1 = np.array([-0.404240685876510, 0.679412484741657,
-                        0.003558952648167, -1.745438223176216e-04, -4.804320406255162])
-# K [-0.426243385650803,0.630959102496925,-1.016065433982113]
-# P [0.002912859612620,0.001469594828381]
-distCoeffs2 = np.array([-0.426243385650803, 0.630959102496925,
-                        0.002912859612620, 0.001469594828381, -1.016065433982113])
-cameraMatrix1 = mtx
-cameraMatrix2 = mtx_r
+distCoeffs1 = None
+distCoeffs2 = None
 
 P1 = None # project matrix
 P2 = None
 
 rt1 = None # R|T matrix
 rt2 = None
+
 
 '''
 # some ERROR 
@@ -42,14 +23,9 @@ R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(
     (640, 480), R, T)
 '''
 
-
-def getrtMtx(rvec,tvec):
-    rmtx, _ = cv2.Rodrigues(rvec)
-    return np.hstack((rmtx,tvec))
-
-def computeProjectMtx(undistort=False):
-    global P1, P2, cameraMatrix1, cameraMatrix2, rt1, rt2
+def calibration():
     # termination criteria
+    global cameraMatrix1, cameraMatrix2, distCoeffs1, distCoeffs2
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -62,19 +38,86 @@ def computeProjectMtx(undistort=False):
     objpoints_r = []
     imgpoints_r = []
 
+    images = glob.glob('../left512/*.bmp')
+    images_r = glob.glob('../right512/*.bmp')
+    #print(images,images_r)
+    images.sort()
+    images_r.sort()
+
+    for fname, fname_r in zip(images, images_r):
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        img_r = cv2.imread(fname_r)
+        gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9, 7), None)
+        #print('corners',corners)
+        ret_r, corners_r = cv2.findChessboardCorners(gray_r, (9, 7), None)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True and ret_r == True:
+            objpoints.append(objp)
+            objpoints_r.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1),
+                                        criteria)
+            corners2_r = cv2.cornerSubPix(gray_r, corners_r, (11, 11), (-1, -1),
+                                          criteria)
+            imgpoints.append(corners2)
+            imgpoints_r.append(corners2_r)
+
+            # Draw and display the corners
+            # cv2.imshow('img', img)
+            # cv2.waitKey(500)
+        else:
+            print("No corners found")
+
+    ret, cameraMatrix1, distCoeffs1, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints,
+                                                       gray.shape[::-1], None,
+                                                       None)
+
+    ret, cameraMatrix2, distCoeffs2, rvecs, tvecs = cv2.calibrateCamera(objpoints_r,
+                                                           imgpoints_r,
+                                                           gray_r.shape[::-1],
+                                                           None, None)
+    #print("mtx mtx_r\n",cameraMatrix1, "\n", cameraMatrix2)
+    #print("dis\n", distCoeffs1, "\n", distCoeffs2)
+    #print("Intrinsic Done.")
+
+def getrtMtx(rvec,tvec):
+    rmtx, _ = cv2.Rodrigues(rvec)
+    return np.hstack((rmtx,tvec))
+
+def computeProjectMtx(undistort=False, pointnumber=4):
+    global P1, P2, cameraMatrix1, cameraMatrix2, rt1, rt2, distCoeffs1, distCoeffs2
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((7 * 9, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:9, 0:7].T.reshape(-1, 2) * 25
+
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane
+
+    objpoints_r = []
+    imgpoints_r = []
+
 
     images = glob.glob('../stereo512/left.bmp')
     images_r = glob.glob('../stereo512/right.bmp')
 
 
     for fname, fname_r in zip(images, images_r):
-        
         img = cv2.imread(fname)
         img_r = cv2.imread(fname_r)
 
         if undistort:
             img = undistortImage(img, cameraMatrix1, distCoeffs1)
-            img_r = undistortImage(img_r, cameraMatrix2, distCoeffs2)
+            img_r = undistortImage(img_r, cameraMatrix2, distCoeffs2)    
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
         # Find the chess board corners
@@ -93,11 +136,17 @@ def computeProjectMtx(undistort=False):
                                           criteria)
             imgpoints.append(corners2)
             imgpoints_r.append(corners2_r)
+    # random choose some points
+    
+    randn_index = np.random.randint(0,63,pointnumber)
 
-    ret, rotation, translation = cv2.solvePnP(objpoints[0], imgpoints[0], 
+    objpoints_ = [objpoints[0][randn_index]]
+    imgpoints_ = [imgpoints[0][randn_index]]
+    imgpoints_r_ = [imgpoints_r[0][randn_index]]
+    ret, rotation, translation = cv2.solvePnP(objpoints_[0], imgpoints_[0], 
     cameraMatrix1, distCoeffs1)
 
-    ret, rotation_r, translation_r = cv2.solvePnP(objpoints[0], imgpoints_r[0], 
+    ret, rotation_r, translation_r = cv2.solvePnP(objpoints_[0], imgpoints_r_[0], 
     cameraMatrix2, distCoeffs2)
 
     rt1 = getrtMtx(rotation, translation)
@@ -110,20 +159,21 @@ def computeProjectMtx(undistort=False):
     l = imgpoints[0].reshape(63,2).T
     r = imgpoints_r[0].reshape(63,2).T
 
-    print("left cam pixel\n", l.shape, l)
-    print("right cam pixel\n", r.shape, r) 
+    #print("left cam pixel\n", l.shape, l)
+    #print("right cam pixel\n", r.shape, r) 
     
-    print("P1\n",P1, "\nP2:\n",P2)
+    #print("P1\n",P1, "\nP2:\n",P2)
     p4d = cv2.triangulatePoints(P1, P2, l, r)
-    print("left camear p4d\n", p4d/p4d[-1])
+    #print("left camear p4d\n", p4d/p4d[-1])
 
     # check rt1 
     rtmtxl_homo = np.vstack((rt1,np.array([0,0,0,1])))
     obj_homo = cv2.convertPointsToHomogeneous(objpoints[0]).reshape(63,4).T
-    print("P*RT:\n", np.dot(rtmtxl_homo, obj_homo))
+    #print("P*RT:\n", np.dot(rtmtxl_homo, obj_homo))
     
 
 def getImagePoints(undistort = False):
+    global P1, P2, cameraMatrix1, cameraMatrix2, rt1, rt2, distCoeffs1, distCoeffs2
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -150,7 +200,7 @@ def getImagePoints(undistort = False):
         if undistort:
             img = undistortImage(img, cameraMatrix1, distCoeffs1)
             img_r = undistortImage(img_r, cameraMatrix2, distCoeffs2)
-
+            
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
 
@@ -197,17 +247,35 @@ def getp3d(imgpoints_l, imgpoints_r):
     return X[:-1]
     
 
+import matplotlib.pyplot as plt  
+def plot_mse(pn,_x,_y,_z):
+
+    l1=plt.plot(pn,_x,'r--',label='X')
+    l2=plt.plot(pn,_y,'g--',label='Y')
+    l3=plt.plot(pn,_z,'b--',label='Z')
+    plt.plot(pn,_x,'ro-',pn,_y,'g+-',pn,_z,'b^-')
+    plt.title('MSE-Different Point Number for RT')
+    plt.xlabel('The point number compute RT')
+    plt.ylabel('The MSE')
+    plt.legend()
+    plt.show()
+
+
 if __name__ == '__main__':
     objp = np.zeros((7 * 9, 3), np.float32)
     objp[:, :2] = np.mgrid[0:9, 0:7].T.reshape(-1, 2) * 25
     objp = objp.T
-
-    computeProjectMtx(undistort=False)
+ 
+    calibration()
+    computeProjectMtx(undistort = False,pointnumber=63)
     # check for cheeseboard 
     l, r = getImagePoints(undistort=False)
     p3d = getp3d(l, r)
+
     print("cheese board corners p3d:\n", p3d)
-    print("MSE: ",np.sqrt(np.sum(np.square(p3d - objp)/63,axis=1)))
+    print("MSE: ",np.sqrt(np.sum(np.square(p3d - objp)/63, axis = 1)))
+    print("MSE2: ",np.sqrt(np.sum(np.square(p3d[-1]-0)/63)))
+
 
     # experiments  ../stereo512/left.bmp
     l = np.array([[432.0, 378.0], [244.0, 168.0]])
@@ -215,3 +283,25 @@ if __name__ == '__main__':
     p3d = getp3d(l, r)
     print("p3d:")
     print(p3d)
+
+    # RT 矩阵与点数的关系
+    l, r = getImagePoints(undistort=True)
+    pn = [(i+4) for i in range(10)]
+    _x, _y, _z= [], [], []
+    loop = 100
+    for _ in pn:
+        tmp = np.array([0., 0., 0.])
+        for i in range(loop):
+            computeProjectMtx(undistort = True, pointnumber=_)
+            p3d = getp3d(l, r)
+            #print("cheese board corners p3d:\n", p3d)
+            tmp += np.sqrt(np.sum(np.square(p3d-objp)/63, axis=1))
+        tmp = list(tmp/loop)
+        _x.append(tmp[0])
+        _y.append(tmp[1])
+        _z.append(tmp[2])
+    plot_mse(pn,_x,_y,_z)
+    
+
+    
+
